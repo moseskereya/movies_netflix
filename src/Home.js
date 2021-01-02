d7d9bbfb0dc6504a6e81148308783f0eda3c38d0
Moses <lazaromoses77@gmail.com>
2021-01-02 11:00:02 +0300

    changing styles

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Movie from './Components/Movie';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import {HashRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from "./Components/context"
 import Show from "./Components/Show"
 import Detail from "./Components/MovieDetail"
