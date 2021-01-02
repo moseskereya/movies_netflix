b87cb9147b92fb242be017e80b3c50a4fbbaa858
Moses <lazaromoses77@gmail.com>
2021-01-02 09:58:29 +0300

    Updates

---
+++ precache-manifest.6f9421abbdc01aca67463882a0b6f334.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cac4ea71f34b86227ae4dae682e2466",
+    "url": "/movies_netflix/index.html"
+  },
+  {
+    "revision": "bfbf13d1f61ed91c946a",
+    "url": "/movies_netflix/static/css/main.dcae63a3.chunk.css"
+  },
+  {
+    "revision": "abc84b0b1412d534b96a",
+    "url": "/movies_netflix/static/js/2.90be7543.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movies_netflix/static/js/2.90be7543.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfbf13d1f61ed91c946a",
+    "url": "/movies_netflix/static/js/main.4c8f397d.chunk.js"
+  },
+  {
+    "revision": "035ec39e70e78ca63551",
+    "url": "/movies_netflix/static/js/runtime-main.1c82af99.js"
+  },
+  {
+    "revision": "14fbc4492e79bbfc4db880f45a8fec99",
+    "url": "/movies_netflix/static/media/user.14fbc449.jpg"
+  }
+]);
